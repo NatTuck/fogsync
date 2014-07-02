11351ee8458558a56994eb00c68bccabbf9f259e
Nat Tuck <nat@ferrus.net>
2014-07-01 22:03:56 -0400

    Not entirely sure why /shares isn't getting served in webui.

--- src/webui/shares.go
+++ src/webui/shares.go
@@ -4,20 +4,28 @@ package webui
 import (
 	"net/http"
 	"encoding/json"
+	"fmt"
 	"../shares"
 	"../fs"
 )
 
 type Shares struct {
-	Shares []*shares.Share `json:"shares"`
+	Shares []*shares.ShareConfig `json:"shares"`
 }
 
 func serveShares(ww http.ResponseWriter, req *http.Request) {
 	hdrs := ww.Header()
 	hdrs["Content-Type"] = []string{"application/json"}
 
-	shares := shares.GetMgr().List()
-	resp := Shares{shares}
+	fmt.Println("XX - Serving shares")
+
+	cfgs := make([]*shares.ShareConfig, 0)
+	for _, ss := range(shares.GetMgr().List()) {
+		fmt.Println("XX - Appending share", ss)
+		cfgs = append(cfgs, ss.Config)
+	}
+
+	resp := Shares{cfgs}
 
 	data, err := json.MarshalIndent(&resp, "", "  ")
 	fs.CheckError(err)
