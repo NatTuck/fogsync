014f5bf4af9f0abefc14abd91f02ea2bfd5c2987
Nat Tuck <nat@ferrus.net>
2014-08-10 14:00:48 -0400

    Add a concept of app version.

---
+++ src/fogsync/version.go
@@ -0,0 +1,2 @@
+package main
+var BUILD_ID = "2014-08-10 13:45 nat@alice"
--- src/webui/server.go
+++ src/webui/server.go
@@ -2,6 +2,9 @@ package webui
 
 import (
 	"net/http"
+	"encoding/json"
+	"../config"
+	"../fs"
 )
 
 func Start() {
@@ -12,7 +15,25 @@ func serverLoop() {
 	http.HandleFunc("/", serveAssets)
 	http.HandleFunc("/shares/", serveShares)
 	http.HandleFunc("/settings/", serveSettings)
+	http.HandleFunc("/about/", serveAbout)
 
 	http.ListenAndServe(":5000", nil)
 }
 
+type AboutInfo struct {
+	Version string
+}
+
+func serveAbout(ww http.ResponseWriter, req *http.Request) {
+	ainfo := &AboutInfo{
+		Version: config.VERSION,
+	}
+
+	hdrs := ww.Header()
+	hdrs["Content-Type"] = []string{"application/json"}
+
+	data, err := json.MarshalIndent(&ainfo, "", "  ")
+	fs.CheckError(err)
+
+	ww.Write(data)
+}
